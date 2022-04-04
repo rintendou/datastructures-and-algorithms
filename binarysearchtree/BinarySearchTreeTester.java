4c369177b54d14a96a7d9e45a92fed8e24c2ad5f
Danny Chen <dannychen8102@gmail.com>
2022-04-03 21:20:33 -0400

    Tested allow methods, many are not functional. Debugging ASAP

--- binarysearchtree/BinarySearchTreeTester.java
+++ binarysearchtree/BinarySearchTreeTester.java
@@ -4,6 +4,7 @@ public class BinarySearchTreeTester {
     public static void main(String[] args) {
         Node root = new Node(20);
         BinarySearchTree tree = new BinarySearchTree(root);
+
         tree.insert(10);
         tree.insert(30);
         tree.insert(80);
@@ -14,13 +15,34 @@ public class BinarySearchTreeTester {
         System.out.println("---------" + '\n' + "Printing current values of the tree: ");
         tree.printValues(root);
 
+        System.out.println("---------" + '\n' + "Returning # of nodes in tree: ");
+        System.out.println(tree.getNodeCount()); // Not printing the correct thing, debugging.
+
         System.out.println("---------");
         tree.delete(root, 30);
         System.out.println("Removing node containing value 30: ");
         tree.printValues(root);
 
+        System.out.println("---------" + '\n' + "Is the tree a BST?: ");
+        System.out.println(tree.isBinarySearchTree(root, 0, Integer.MAX_VALUE));
+        
+        System.out.println("---------" + '\n' + "Max value in the tree: ");
+        System.out.println(tree.getMax(root)); // Not printing the correct thing, debugging.
+
+        System.out.println("---------" + '\n' + "Minimum value in the tree: ");
+        System.out.println(tree.getMin(root)); // Not printing the correct thing, debugging.
+
+        System.out.println("---------" + '\n' + "Is a value in the BST?: ");
+        System.out.println(tree.isInTree(5)); // Not printing the correct thing, debugging.
+
         System.out.println("---------" + '\n' + "Returning # of nodes in tree: ");
         System.out.println(tree.getNodeCount()); // Not printing the correct thing, debugging.
 
+        System.out.println("---------" + '\n' + "Successor of node 'x': ");
+        System.out.println(tree.getSuccessor(5)); // Not printing the correct thing, debugging.
+
+        System.out.println("---------" + '\n' + "Deleting tree: ");
+        tree.deleteTree(); // Does not work.
+        tree.printValues(root);
     }
 }
